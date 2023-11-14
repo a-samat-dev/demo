0161e08cd8be30fea474641399f285a046f635aa
samatabibulla <a.samat.dev@gmail.com>
2023-11-14 07:05:52 +0600

    1

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        log.info("Incoming request");
+        return "test v1";
+    }
+}
